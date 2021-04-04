72542da4779dab97da4c0a6c0b108c33713b93f2
sanengineer <48153104+sanengineer@users.noreply.github.com>
2021-04-04 14:39:54 +0700

    update readme

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 On almost engineer just strating, swift  familliar running on apple device on client-side. But since apple build SwiftNIO, swift unlock capability can running on server-side. In this project I try use vapor to create API Gateway for microservices architexture. vapor is web framework for server-side swift, and build on top of SwiftNIO.
 
+
+## API Gateway For
+
+### User Services
+    
+[swift-postgres-redis-user-services](https://github.com/sanengineer/microservices-swift-postgresql-user-services)
+
+### Product Services
+
+[swift-mysql-product-services](https://github.com/sanengineer/microservices-swift-postgresql-product-services)
+
--- Sources/App/Controllers/ProductController.swift
+++ Sources/App/Controllers/ProductController.swift
@@ -8,15 +8,19 @@ struct ProductController: RouteCollection {
         productServiceUrl = "http://\(productServiceHostName):5689"
     }
     
+    
     func boot(routes: RoutesBuilder) throws {
         let productRouteGroup = routes.grouped("api", "v1", "product")
         
         productRouteGroup.get( use: getAllHandler)
         productRouteGroup.get( ":product_id", use: getOneHandler)
         productRouteGroup.get( "count", use: getCountHandler)
+        productRouteGroup.get( "result", use: getSearchHandler)
+        
         productRouteGroup.post( use: createHandler)
         productRouteGroup.put(":product_id", use: updateHandler)
         productRouteGroup.delete(":product_id", use: deleteHandler)
+        
     }
     
     
@@ -26,50 +30,55 @@ struct ProductController: RouteCollection {
     
     
     func getOneHandler(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-        
         let id = try req.parameters.require("product_id", as: UUID.self)
-        
         return req.client.get("\(productServiceUrl)/product/\(id)")
     }
     
+    
     func getCountHandler(_ req: Request) -> EventLoopFuture<ClientResponse> {
-        
         return req.client.get("\(productServiceUrl)/product/count")
     }
     
+    
+    func getSearchHandler(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+
+        guard let searchQuery = req.query[String.self, at: "search_query"] else { throw Abort(.badRequest) }
+
+        return req.client.get("\(productServiceUrl)/product/result?search_query=\(searchQuery)")
+    }
+    
+    
     func createHandler(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
-
         return req.client.post("\(productServiceUrl)/product") { createRequest in
-
             guard let authHeader = req.headers[.authorization].first else {
                 throw Abort(.unauthorized)
             }
-
             createRequest.headers.add(name: .authorization, value: authHeader)
             try createRequest.content.encode(req.content.decode(CreateProductData.self))
         }
     }
+    
     
     func updateHandler(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
         let productId = try req.parameters.require("product_id", as: UUID.self)
         
         return req.client.put("\(productServiceUrl)/product/\(productId)") { updateReq in
             guard let authHeader = req.headers[.authorization].first else { throw Abort(.unauthorized ) }
-            
             updateReq.headers.add(name: .authorization, value: authHeader)
             try updateReq.content.encode(req.content.decode(CreateProductData.self))
         }
     }
+    
     
     func deleteHandler(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
         let productId = try req.parameters.require("product_id", as: UUID.self)
         
         return req.client.delete("\(productServiceUrl)/product/\(productId)") { deleteReq in
             guard let authHeader = req.headers[.authorization].first else { throw Abort(.unauthorized) }
-            
             deleteReq.headers.add(name: .authorization, value: authHeader)
         }
     }
+    
     
 }
 
@@ -78,4 +87,8 @@ struct CreateProductData: Content {
     let descriptions: String
     let price: Int
     let image_featured: String
+}
+
+struct SearchData: Content {
+    let value: String
 }
