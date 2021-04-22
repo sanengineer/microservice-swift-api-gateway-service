52d0cd621a0a91c10acd0c0441115920fb2d03b9
sanengineer <48153104+sanengineer@users.noreply.github.com>
2021-04-22 23:54:07 +0700

    add varian services

---
+++ Sources/App/Controllers/VarianController.swift
@@ -0,0 +1,45 @@
+import Vapor
+
+struct VarianController: RouteCollection {
+    let varianServiceUrl: String
+    
+    init(varianServiceHostname: String, varianServicePort: String) {
+        varianServiceUrl = "http://\(varianServiceHostname):\(varianServicePort)"
+    }
+    
+    
+    func boot(routes: RoutesBuilder) throws {
+        let varianRouteGroup = route.grouped("api", "v1", "varian")
+        
+        varianRouteGroup.get
+    }
+    
+    
+    func getAllVarian(_ req: Request) -> EventLoopFuture<ClientResponse> {
+        return req.client.get("\(varianServiceUrl)/varian")
+    }
+    
+    
+    func createOneVarian(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+        return req.client.post("\(varianServiceUrl)/varian"){
+            createRequest in
+            
+            guard let authHeader = req.headers[.authorization].first else {
+                throw Abort(.unauthorized)
+            }
+            
+            createRequest.headers.add(name: .authorization, value: authHeader)
+            
+            try createRequest.content.encode(req.content.decode(CreateVarianData.self))
+        }
+    }
+    
+    
+    
+}
+
+struct CreateVarianData: Content {
+    let size: String
+    let sugar: String
+    let ice: String
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,11 +5,13 @@ func routes(_ app: Application) throws {
     let productHostname: String
     let orderHostname: String
     let cartHostname: String
+    let varianHostname: String
     
     let userPort: String = Environment.get("USER_PORT")!
     let cartPort: String = Environment.get("CART_PORT")!
     let productPort: String = Environment.get("PRODUCT_PORT")!
     let orderPort: String = Environment.get("ORDER_PORT")!
+    let varianPort: String = Environment.get("VARIAN_PORT")!
     
     if let userEnvHostname = Environment.get("USER_HOSTNAME"){
         userHostname = userEnvHostname
@@ -17,11 +19,13 @@ func routes(_ app: Application) throws {
         userHostname = "localhost"
     }
     
+    
     if let productEnvHostname = Environment.get("PRODUCT_HOSTNAME"){
         productHostname = productEnvHostname
     } else {
         productHostname = "localhost"
     }
+    
     
     if let orderEnvHostname = Environment.get("ORDER_HOSTNAME"){
         orderHostname = orderEnvHostname
@@ -34,6 +38,13 @@ func routes(_ app: Application) throws {
         cartHostname = cartEnvHostname
     } else {
         cartHostname = "localhost"
+    }
+    
+    
+    if let varianEnvHostname = Environment.get("VARIAN_HOSTNAME"){
+        varianHostname = varianEnvHostname
+    } else {
+        varianHostname = "localhost"
     }
     
     
