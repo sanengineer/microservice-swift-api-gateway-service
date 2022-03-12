0b2d8aa48534841c84595910ff690fc083565da1
sanengineer <48153104+sanengineer@users.noreply.github.com>
2022-03-12 21:20:52 +0700

    update on routes and controller: - add env category Url control flow - register routes category - add logic routes category

--- Sources/App/Controllers/CategoryController.swift
+++ Sources/App/Controllers/CategoryController.swift
@@ -3,20 +3,36 @@ import Vapor
 struct CategoryController: RouteCollection {
     let categoryServiceUrl: String
     
-    init(categoryServicesHostname: String, categoryServicesPort: String) {
-        categoryServiceUrl = "http://\(categoryServicesHostname):\(categoryServicesPort)"
+    init(_categoryServiceUrl: String) {
+        categoryServiceUrl = "\(_categoryServiceUrl)"
     }
     
     func boot(routes: RoutesBuilder) throws {
         let categoryGroup = routes.grouped("api", "v1", "category")
      
-        categoryGroup.get( use: getAllCategory)
+        categoryGroup.get(use: getAllCategory)
+        categoryGroup.get("count", use: getNumbersCategory)
         categoryGroup.get(":category_id", use: getOneCategory)
+        categoryGroup.post(use: createOneCategory)
+        categoryGroup.put(":category_id", use: updateOneCategory)
+        categoryGroup.delete(":category_id", use: deleteOneCategory)
     }
     
     func getAllCategory(_ req: Request) -> EventLoopFuture<ClientResponse> {
         
-        return req.client.get("\(categoryServiceUrl)/category/"){
+        return req.client.get("\(categoryServiceUrl)/category"){
+            getRequest in
+            guard let authHeader = req.headers[.authorization].first else {
+                throw Abort(.unauthorized)
+            }
+            
+            getRequest.headers.add(name: .authorization, value: authHeader)
+        }
+    }
+
+    func getNumbersCategory(_ req: Request) -> EventLoopFuture<ClientResponse> {
+        
+        return req.client.get("\(categoryServiceUrl)/category/count"){
             getRequest in
             guard let authHeader = req.headers[.authorization].first else {
                 throw Abort(.unauthorized)
@@ -41,4 +57,47 @@ struct CategoryController: RouteCollection {
             getRequest.headers.add(name: .authorization, value: authHeader)
         }
     }
+
+    func createOneCategory(_ req: Request) -> EventLoopFuture<ClientResponse> {
+        
+        return req.client.post("\(categoryServiceUrl)/category"){
+            getRequest in
+            guard let authHeader = req.headers[.authorization].first else {
+                throw Abort(.unauthorized)
+            }
+            
+            getRequest.headers.add(name: .authorization, value: authHeader)
+        }
+    }
+
+    func updateOneCategory(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+        
+        let id = try req.parameters.require("category_id", as: UUID.self)
+        
+        return req.client.put("\(categoryServiceUrl)/category/\(id)"){
+            getRequest in
+            guard let authHeader = req.headers[.authorization].first else {
+                throw Abort(.unauthorized)
+            }
+            
+            getRequest.headers.add(name: .authorization, value: authHeader)
+        }
+    }
+
+    func deleteOneCategory(_ req: Request) throws -> EventLoopFuture<ClientResponse> {
+        
+        let id = try req.parameters.require("category_id", as: UUID.self)
+        
+        return req.client.delete("\(categoryServiceUrl)/category/\(id)"){
+            getRequest in
+            guard let authHeader = req.headers[.authorization].first else {
+                throw Abort(.unauthorized)
+            }
+            
+            getRequest.headers.add(name: .authorization, value: authHeader)
+        }
+    }
+
+
+
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,6 +4,7 @@ import Vapor
 func routes(_ app: Application) throws {
     let userUrl: String
     let productUrl: String
+    let categoryUrl: String
  
     guard let userUrlEnv = Environment.get("USER_URL") else {
         return print("No Env User URL")
@@ -13,13 +14,18 @@ func routes(_ app: Application) throws {
         return print("No Env Product Hostname")
     }
 
+    guard let categoryUrlEnv = Environment.get("CATEGORY_URL") else {
+        return print("No Env Product Hostname")
+    }
+
     userUrl = "\(userUrlEnv)"
     productUrl = "\(productUrlEnv)"
+    categoryUrl = "\(categoryUrlEnv)"
 
     try app.register(collection: UserController(_userServiceUrl: userUrl ))
-
     try app.register(collection: RoleController(_roleServiceUrl: userUrl ))
-    
     try app.register(collection: ProductController(_productServiceUrl: productUrl))
+    try app.register(collection: CategoryController(_categoryServiceUrl: categoryUrl))
+    
     
 }
